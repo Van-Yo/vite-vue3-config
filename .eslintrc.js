dab9b73183943418e97985acc19d1a7d16d9e3f3
wanrong <wanrong@wondersgroup.com>
2021-05-12 16:31:08 +0800

    架构搭建+代码规范

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'plugin:vue/essential',
+        'airbnb-base',
+    ],
+    parserOptions: {
+        ecmaVersion: 12,
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module',
+    },
+    plugins: [
+        'vue',
+        '@typescript-eslint',
+    ],
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
+        },
+    },
+    rules: {
+        indent: ['error', 4], // eslint缩进四格
+        'import/extensions': [2, 'never', { 'web.js': 'never', json: 'never' }],
+        'import/no-extraneous-dependencies': [2, {
+            devDependencies: true,
+            peerDependencies: true,
+            // optionalDependencies: true,
+            // bundledDependencies: true
+        }],
+        'import/no-unresolved': [
+            2,
+            { caseSensitive: false },
+        ],
+    },
+};
---
+++ src/views/home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home-area">
+        home
+        <el-button type="primary" @click="$store.commit('increment')">{{
+            $store.state.count
+        }}</el-button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Home',
+});
+</script>
+
+<style lang="less">
+@bg: #f0f0f0;
+.home-area {
+    background: @bg;
+}
+</style>
